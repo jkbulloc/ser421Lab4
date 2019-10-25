52150d23e6a7180ed020e786cd1f27094284b403
jkbulloc <jkbulloc@asu.edu>
2019-10-25 16:40:33 -0700

    renamed html file and added activity1.js

---
+++ Activity1.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// 1: Output to the console the <ol> element encompassing the results of the search
+console.log(document.getElementById("b_results"));
+
+//2 Output to the console the code for the "onload" event on the <body> element
+
