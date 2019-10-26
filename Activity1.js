4aa6897edf09028d0d992c402bbb46e6c1f59a93
jkbulloc <jkbulloc@asu.edu>
2019-10-25 18:45:29 -0700

    re-loaded original version of html page and saved step 6 to .js file

--- Activity1.js
+++ Activity1.js
@@ -7,9 +7,13 @@ console.log(document.getElementById("b_results"));
 console.log(document.body.onload);
 
 //3 Output to the console the 2nd child node underneath the <body> element
+console.log(document.body.childNodes[1]);
 
 //4 Output to the console the number of <h2> tags in the page
+console.log(document.getElementsByTagName("h2").length);
 
 //5 Output to the console the value in the search bar
+console.log(document.getElementById("sb_form_q").value);
 
 //6 make the "add bing new tab extension" text in the upper right corner result go away
+document.getElementsByClassName("b_op_atxt")[0].innerHTML = "";
