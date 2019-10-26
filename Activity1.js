4a151e28403be5aacdb163b95b2a8340e43a1c36
jkbulloc <jkbulloc@asu.edu>
2019-10-25 18:42:16 -0700

    removed original (missing files) and added completed activity 1 html doc

--- Activity1.js
+++ Activity1.js
@@ -1,7 +1,15 @@
 "use strict";
 
-// 1: Output to the console the <ol> element encompassing the results of the search
+//1 Output to the console the <ol> element encompassing the results of the search
 console.log(document.getElementById("b_results"));
 
 //2 Output to the console the code for the "onload" event on the <body> element
+console.log(document.body.onload);
 
+//3 Output to the console the 2nd child node underneath the <body> element
+
+//4 Output to the console the number of <h2> tags in the page
+
+//5 Output to the console the value in the search bar
+
+//6 make the "add bing new tab extension" text in the upper right corner result go away
