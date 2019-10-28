99cce8524c45ee63c8104b3b9c3f2e49536eee28
jkbulloc <jkbulloc@asu.edu>
2019-10-28 06:34:50 -0700

    activity2 stuff

---
+++ Activity2/Activity2.js
@@ -0,0 +1,9 @@
+"use strict";
+// const DOMParser = require("dom-parser");
+// const parser = new DOMParser();
+
+//var doc = parser.parseFromString("./Activity2.html", "text/html");
+
+function test (){
+    console.log(document.getElementsByTagName("div"));
+}
