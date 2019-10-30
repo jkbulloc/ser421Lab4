063ea0ea3e301b3b93ad960a76541b439e4f3c54
jkbulloc <jkbulloc@asu.edu>
2019-10-29 18:21:38 -0700

    activity 2 functionality and added dictionary.json

--- Activity2/Activity2.js
+++ Activity2/Activity2.js
@@ -1,9 +1,26 @@
 "use strict";
-// const DOMParser = require("dom-parser");
-// const parser = new DOMParser();
+const review = "Joker review: Joker isn’t just an awesome comic book movie, it’s an awesome movie, period. " +
+"It offers no easy answers to the unsettling questions it raises about a cruel society in decline. " + 
+"Joaquin Phoenix’s fully committed performance and Todd Phillips’ masterful albeit loose reinvention of the DC source material make Joker a film that should leave comic book fans and non-fans alike disturbed and moved in all the right ways." +
+" - IGN.com"
 
-//var doc = parser.parseFromString("./Activity2.html", "text/html");
+function greetUser(){
+    let name = document.getElementById("user_name").value;
+    if (name){
+        let greeting = "Hello, " + name + " Welcome to the movie review forum! Please enter a coment about the movie!"
+        document.getElementById("greeting").innerHTML = greeting;
+        document.getElementById("review").innerHTML = review;
+    } else {
+        alert("Please enter a valid name!");
+    }
+}
 
-function test (){
-    console.log(document.getElementsByTagName("div"));
+function getComments (){
+    let comments = document.getElementById("comments").value;
+    try {
+        let obj = JSON.parse(comments);
+        alert(obj.key);
+    } catch {
+        alert("doesn't work");
+    }
 }
