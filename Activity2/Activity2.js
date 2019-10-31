07dd4b66058de17be595233f7dd336bd30ff2337
jkbulloc <jkbulloc@asu.edu>
2019-10-31 11:22:02 -0700

    replace functionality working

--- Activity2/Activity2.js
+++ Activity2/Activity2.js
@@ -23,8 +23,8 @@ function load(){
 
 function greetUser(){
     name = document.getElementById("user_name").value;
-    document.cookie = "username=" + name;
-    console.log(document.cookie);
+    // document.cookie = "username=" + name;
+    // console.log(document.cookie);
     if (name){
         loadHomePage();
     } else {
@@ -38,29 +38,44 @@ function getComments (){
         let obj = JSON.parse(comments);
         alert(obj.key);
     } catch {
-        search(comments);
+        let words = comments.split(/\s|\.|,\s/)
+        console.log(words);
+        display(words);
     }
 }
 
-function search(input){
-    let words = input.split(/[. , ]/);
-    let entries;
+function display(words) {
+    let newSentence = "";
+    let check;
     for (let i in words){
-        if (words[i] != ""){
-            entries = dictionary.entries;
-            for (let j in entries) {
-                for(let k in entries[j].key){
-                    if (words[i] === entries[j].key[k]){
-                        document.getElementById("comments").value = "Values have been replaced";
-                    }
-                }
+        if (words[i]!= ""){
+            check = search(words[i])
+            if(check){
+                newSentence += replace(check) + " ";
+            } else {
+                newSentence += words[i] + " ";
             }
         }
     }
+    document.getElementById("comments").value = newSentence;
 }
 
-function replace(words, index, keyIndex, arrayIndex){
-    
+function search(word){
+    let ret = false;
+    let entries = dictionary.entries;
+    for (let keyIndex in entries) {
+        for(let keyWord in entries[keyIndex].key){
+            if (word === entries[keyIndex].key[keyWord]){
+                ret = keyIndex;
+            }
+        }
+    }
+    return ret;
+}
+
+function replace(keyIndex){
+    let randomNum = Math.floor(Math.random() * Math.floor(dictionary.entries[keyIndex].answer.length - 1));
+    return dictionary.entries[keyIndex].answer[randomNum];
 }
 
 var dictionary = {
